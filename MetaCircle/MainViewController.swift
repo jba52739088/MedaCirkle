f7cd1e3a153414cdd23d1cf83b8228b1215558e0
En-Yu, Huang <jba52739088@gmail.com>
2022-05-14 16:29:29 +0800

    UPDATED: create development environment ADDED: pods, RX, R.swift, swiftyJSON

---
+++ MetaCircle/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MetaCircle
+//
+//  Created by 黃恩祐 on 2022/5/14.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      self.view.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
